0cc38236be7db8752e4a9a8cbe477f0205ec2fa7
Nathan Schmidt <nschmdt@gmail.com>
2021-11-29 12:01:45 -0600

    Update Course Model

---
+++ models/course.js
@@ -0,0 +1,52 @@
+'use strict'
+
+const {Model, DataTypes} = require('sequelize');
+
+module.exports = (sequelize) => {
+    class Course extends Model {
+    }
+
+    Course.init({
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    msg: 'Please, enter a title for the course'
+                }
+            }
+        },
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    msg: 'Please, enter a course description'
+                }
+            }
+        },
+        estimatedTime: {
+            type: DataTypes.STRING
+        },
+        materialsNeeded: {
+            type: DataTypes.STRING
+        }
+    }, {sequelize});
+
+    Course.associate = (models) => {
+        Course.belongsTo(models.User, {
+            as: 'user',
+            foreignKey: {
+                fieldName: 'userId',
+                allowNull: false
+            }
+        });
+    };
+
+    return Course;
+}
